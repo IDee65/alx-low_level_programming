d9a90846635e7823d687884b844f096c1b34d2eb
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-08 16:18:45 +0100

    printing a string with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,8 @@
  */
 int main(void)
 {
-/**
- *char string[] = "\"Programming is like building a multilingual puzzle";
- *puts(string);
-*/
+
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
