a4d623251db4efde3fee658ae694c3956f3133d7
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-10 18:32:00 +0100

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alp;
+
+for (alp = 'z'; alp >= 'a'; alp--)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
