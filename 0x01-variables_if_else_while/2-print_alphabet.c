3f62961217b8d68216fb2d18022b41620903d7a5
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-10 15:48:18 +0100

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alp;
+
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
