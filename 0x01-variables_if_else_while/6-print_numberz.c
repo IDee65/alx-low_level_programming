9eb97d0503d4b89e65fa2efb6b14b78b9b4c59af
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-10 18:26:20 +0100

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+for (; num < 10; num++)
+putchar('0' + num);
+putchar('\n');
+return (0);
+}
