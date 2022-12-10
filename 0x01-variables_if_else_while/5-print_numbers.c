f8b01da85f27300e931c2189180ef16696e05bf1
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-10 16:13:42 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 0;
+for (; num <= 10; num++)
+printf("%d\n", num);
+return (0);
+}
