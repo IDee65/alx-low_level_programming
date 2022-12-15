88fd2593b30c2f0330c2349e43de15500ca2b255
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2022-12-15 20:19:37 +0100

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,26 @@
  */
 int main(void)
 {
-int i = 0;
-for (; i <= 100; i++)
+int i = 1;
+
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0)
+if (i % 15 == 0)
+printf("FizzBuzz");
+
+else if (i % 3 == 0)
 printf("Fizz");
+
 else if (i % 5 == 0)
 printf("Buzz");
-else if (i % 15 == 0)
-printf("FizzBuzz");
+
 else
 printf("%d", i);
+
 if (i != 100)
 printf(" ");
 }
+
 printf("\n");
 return (0);
 }
