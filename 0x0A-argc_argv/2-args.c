8730aed52aae3defda674a0b2c67f7c56803908b
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2023-01-06 12:54:36 +0100

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments passed to a program on separate lines
+ * @argc: number of command line arguments
+ * @argv: array of strings containing command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
