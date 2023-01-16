99caed1f35d6da3d17a7182f4a5ceb936e4c52fc
Idemudia Agbonghae <idemudia.agbonghae@iisysgroup.com>
2023-01-16 20:45:01 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
